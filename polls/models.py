2a5e4a1eb78d1ae7a509cb9d7754021a29cc8736
aaimsh <aaimsh101@gmail.com>
2017-10-24 17:46:08 +0300

    .

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,5 @@
+import datetime
+from django.utils import timezone
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 # Create your models here.
@@ -10,6 +12,9 @@ class Question(models.Model):
 
     def __str__(self):
     	return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 @python_2_unicode_compatible
--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,12 @@
+import daytime
 from django.test import TestCase
-
+from django.utils import timezone
+from .models import Question
 # Create your tests here.
+
+
+class QuestionModelTests(TestCase):
+	def test_was_published_with_future_question(self):
+		time = timezone.now()+ daytime.timedate(days=30)
+		future_q = Question(pub_date=time)
+		self.assertIs(future_q.was_published_recently(), False)
